23f4fd35b99fa86d030a9e173367097360627ccd
antoniokarl <antonio.karlvincent@auf.edu.ph>
2024-06-10 16:05:39 +0800

    Add files via upload

---
+++ MainPage-2.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module04Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_GoTo_Page(object sender, EventArgs e)
+        {
+            if (sender == Btn_GoTo_TabbedPage)
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+            }
+
+            else if (sender == Btn_GoTo_CarouselPage)
+            {
+                await Navigation.PushAsync(new CarouselPage1());
+            }
+        }
+    }
+}
